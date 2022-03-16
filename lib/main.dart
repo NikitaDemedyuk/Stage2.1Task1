1c71f8ff57e7a64db23f49e237d138ead3f4b0e4
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-16 09:55:57 +0300

    rename text file

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,46 @@
 import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
 
-int main() {
-  Future myFuture = Future(() {
-    print("Hello Future");
-  });
+class ErrorReadFile {
+  String getErrorMessage() {
+    return '\nSome problems with reading file...\n';
+  }
+}
 
-  var myFile = File('/Users/nikita/Developer/Hometask/Programming/Dart/Stage 2.1/Task1(Stage2.1)/FizzBizzText.docx');
+Future<int> readFromFile(File filePath) async {
+    Stream <String> lines = filePath.openRead()
+        .transform(utf8.decoder)       // Decode bytes to UTF-8.
+        .transform(LineSplitter());
 
+    int numberKey = 0;
 
-  print("Main ends");
+    try {
+      await for (var line in lines) {
+        numberKey = int.parse(line);
+      }
+      print('File is now closed.');
+    } catch (e) {
+      print('Error: $e');
+    }
+    return numberKey;
+}
+
+Future <void> writeToFile(File filePath) async {
+  var sink = filePath.openWrite();
+  sink.write('Hello');
+
+  sink.close();
+}
+
+Future <int> main() async {
+  final filePath = File('/Users/nikita/Developer/Hometask/Programming/Dart/Stage 2.1/Task1(Stage2.1)/Input.txt');
+
+  final int numberKey = await readFromFile(filePath);
+
+  stdout.write(numberKey);
+
+  
   return 0;
+
 }
