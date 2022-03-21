223dfa4c31bd12e0ea3ba55296cb9fb7b5f392a2
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-21 22:45:54 +0300

    add changes to main function

--- lib/main.dart
+++ lib/main.dart
@@ -33,82 +33,83 @@ Future<int> readFromFileKeyNumber(File filePath) async {
 }
 
 Future<Map> readFromFileMap(File filePath) async {
-  Stream<String> lines = filePath.openRead()
-      .transform(utf8.decoder)       // Decode bytes to UTF-8.
-      .transform(LineSplitter());
+    Stream<String> lines = filePath.openRead()
+        .transform(utf8.decoder)       // Decode bytes to UTF-8.
+        .transform(LineSplitter());
 
-  Map mapFizzBizzFromFile = Map();
+    Map mapFizzBizzFromFile = Map();
 
-  try {
-    await for (String line in lines) {
-      mapFizzBizzFromFile.addAll({int.parse(line.substring(5, line.indexOf(','))): line.substring(line.indexOf('u') + 4, line.indexOf(';'))});
+    try {
+      await for (String line in lines) {
+        mapFizzBizzFromFile.addAll({int.parse(line.substring(5, line.indexOf(','))): line.substring(line.indexOf('u') + 4, line.indexOf(';'))});
+      }
+    } catch (e) {
+      stdout.write('Error: $e');
     }
-  } catch (e) {
-    stdout.write('Error: $e');
-  }
-  return mapFizzBizzFromFile;
+    return mapFizzBizzFromFile;
 }
 
 Future <void> writeToFile(File? filePath, Map mapFizzBizz) async {
-  var sink = filePath?.openWrite();
-  sink?.write("\n");
-  for (var item in mapFizzBizz.entries) {
-      //stdout.write("${item.key} ${item.value} \n");
-      sink?.write("Key: ${item.key}, Value: ${item.value} \n");
-  }
-  sink?.write("\n");
-  sink?.close();
+    var sink = filePath?.openWrite();
+    sink?.write("\n");
+    for (var item in mapFizzBizz.entries) {
+        //stdout.write("${item.key} ${item.value} \n");
+        sink?.write("Key: ${item.key}, Value: ${item.value} \n");
+    }
+    sink?.write("\n");
+    sink?.close();
 }
 
 void printMap(Map mapFizzBizz) {
-  stdout.write("\n");
-  for (var item in mapFizzBizz.entries) {
-    //stdout.write("Key: ${item.key}, Value: ${item.value} \n");
-    stdout.write("${item.key} ${item.value} \n");
-  }
-  stdout.write("\n");
+    stdout.write("\n");
+    for (var item in mapFizzBizz.entries) {
+      //stdout.write("Key: ${item.key}, Value: ${item.value} \n");
+      stdout.write("${item.key} ${item.value} \n");
+    }
+    stdout.write("\n");
 }
 
 File normalizePath(String rootPath, String fileName) {
-  String filePath = path.join(rootPath, fileName);
-  filePath = path.normalize(filePath);
-  File filePathToRead = new File(filePath);
-  return filePathToRead;
+    String filePath = path.join(rootPath, fileName);
+    filePath = path.normalize(filePath);
+    File filePathToRead = new File(filePath);
+    return filePathToRead;
 }
 
 Future <int> main() async {
 
-  stdout.write('Program start!\n');
-  stdout.write('-------------------------------------------------------------------------------\n');
-  stdout.write('Reading map from a file "InputMap.txt"\n');
+    stdout.write('Program start!\n');
+    stdout.write('-------------------------------------------------------------------------------\n');
+    stdout.write('Reading map from a file "InputMap.txt"\n');
 
-  final File filePathToRead = normalizePath(Directory.current.parent.path, "InputKeyNumber.txt");
-  final File filePathToReadMap = normalizePath(Directory.current.parent.path, "InputMap.txt");
+    final File filePathToReadMap = normalizePath(Directory.current.parent.path, "InputMap.txt");
+    final mapFizzBizzFromFile = readFromFileMap(filePathToReadMap);
 
-  final keyNumbers =  readFromFileKeyNumber(filePathToRead);
+    final File filePathToRead = normalizePath(Directory.current.parent.path, "InputKeyNumber.txt");
+    final numberKeyFromFile =  readFromFileKeyNumber(filePathToRead);
 
-  Map mapFizzBizz = await readFromFileMap(filePathToReadMap);
+    final Map mapFizzBizz = await mapFizzBizzFromFile;
 
-  try {
-    final int numberKey = await keyNumbers;
-    stdout.write('\nKey number from file "InputKeyNumber.txt" : $numberKey\n');
-    if (numberKey < 1 || numberKey > 100) {
-      throw KeyNumberException();
+    try {
+      final int numberKey = await numberKeyFromFile;
+      stdout.write('\nKey number from file "InputKeyNumber.txt" : $numberKey\n');
+      if (numberKey < 1 || numberKey > 100) {
+        throw KeyNumberException();
+      }
+      stdout.write("\nKey: $numberKey, Value: ${mapFizzBizz[numberKey]}\n");
+    } on KeyNumberException catch(e) {
+      stdout.write('Error: ${e.getErrorMessage()}');
+      return 0;
     }
-    stdout.write("\nKey: $numberKey, Value: ${mapFizzBizz[numberKey]}\n");
-  } on KeyNumberException catch(e) {
-    stdout.write('Error: ${e.getErrorMessage()}');
+
+    stdout.write('-------------------------------------------------------------------------------\n');
+
+    stdout.write('Writing map to a file "Output.txt"\n');
+    final File? filePathToWrite = normalizePath(Directory.current.parent.path, "Output.txt");
+    writeToFile(filePathToWrite, mapFizzBizz);
+
+    stdout.write('-------------------------------------------------------------------------------\n');
+
+    stdout.write('End of program\n');
     return 0;
-  }
-
-  stdout.write('-------------------------------------------------------------------------------\n');
-  
-  stdout.write('Writing map to a file "Output.txt"\n');
-  final File? filePathToWrite = normalizePath(Directory.current.parent.path, "Output.txt");
-  writeToFile(filePathToWrite, mapFizzBizz);
-
-  stdout.write('-------------------------------------------------------------------------------\n');
-
-  stdout.write('End of program\n');
-  return 0;
 }
